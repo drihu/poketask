8ced9375737ce30ff6927385f27bd4bd9c718198
Ricardo Huamani <ricardohuamanip@gmail.com>
2018-04-08 03:32:29 -0500

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-
 const fs = require('fs');
 const path = require('path');
 const co = require('co');
 const ejs = require('ejs');
 const program = require('commander');
 const prompt = require('co-prompt');
-const utils = require('./utils');
+const utils = require('../utils');
 
 const location = process.cwd();
 const packageObj = utils.jsonFileToObj(path.join(location, 'package.json'));
+const gitignoreTemplatesFolder = path.join(__dirname, '../gitignore');
+const licenseTemplatesFolder = path.join(__dirname, '../choosealicense.com/_licenses');
 
 program
   .version('0.3.3', '-v, --version');
@@ -40,26 +41,26 @@ program
         ? (yield prompt(`license: (${packageObj.license}) `)) || packageObj.license
         : yield prompt('license: ');
 
-      const keywords = utils.toArrayOfWords(yield prompt('gitignore workspace: '));
+      const gitkeywords = utils.toArrayOfWords(yield prompt('gitignore template: '));
 
       console.log('--');
 
       // Generate .gitignore
-      fs.readdir(path.join(__dirname, 'gitignore'), (err, files) => {
+      fs.readdir(gitignoreTemplatesFolder, (err, templates) => {
         if (err) throw err;
 
-        const gitignore = path.join(location, '.gitignore');
-        fs.writeFileSync(gitignore, '');
+        const gitignoreFile = path.join(location, '.gitignore');
+        fs.writeFileSync(gitignoreFile, '');
 
-        keywords.forEach((keyword) => {
-          const keywordRegExp = utils.toRegExp(`${keyword}.gitignore`);
+        gitkeywords.forEach((gitkeyword) => {
+          const gitkeywordRegExp = utils.toRegExp(`${gitkeyword}.gitignore`);
 
-          files.forEach((file) => {
-            if (keywordRegExp.test(file)) {
-              const filePlace = path.join(__dirname, `gitignore/${file}`);
-              const fileData = fs.readFileSync(filePlace, 'utf8');
+          templates.forEach((template) => {
+            if (gitkeywordRegExp.test(template)) {
+              const templatePlace = path.join(gitignoreTemplatesFolder, template);
+              const templateData = fs.readFileSync(templatePlace, 'utf8');
 
-              fs.appendFile(gitignore, fileData, (e) => {
+              fs.appendFile(gitignoreFile, templateData, (e) => {
                 if (e) throw e;
               });
             }
@@ -71,30 +72,30 @@ program
 
       // Generate .editorconfig
       utils.copyFile(
-        path.join(__dirname, 'resources/.editorconfig'),
+        path.join(__dirname, '../assets/.editorconfig'),
         path.join(location, '.editorconfig'),
       );
 
       // Generate .gitattributes
       utils.copyFile(
-        path.join(__dirname, 'resources/.gitattributes'),
+        path.join(__dirname, '../assets/.gitattributes'),
         path.join(location, '.gitattributes'),
       );
 
       // Generate LICENSE
-      fs.readdir(path.join(__dirname, 'choosealicense.com/_licenses'), (err, files) => {
+      fs.readdir(licenseTemplatesFolder, (err, templates) => {
         if (err) throw err;
         const licenseRegExp = utils.toRegExp(`${license}.txt`);
 
-        files.forEach((file) => {
-          if (licenseRegExp.test(file)) {
-            const licensePlace = path.join(__dirname, `choosealicense.com/_licenses/${file}`);
-            const licenseData = fs.readFileSync(licensePlace, 'utf8')
+        templates.forEach((template) => {
+          if (licenseRegExp.test(template)) {
+            const templatePlace = path.join(licenseTemplatesFolder, template);
+            const templateData = fs.readFileSync(templatePlace, 'utf8')
               .replace(/^---[\s\S]*---\n\n/gi, '')
               .replace(/\[year\]/gi, new Date().getFullYear())
               .replace(/\[fullname\]/gi, author);
 
-            fs.writeFile(path.join(location, 'LICENSE'), licenseData, (e) => {
+            fs.writeFile(path.join(location, 'LICENSE'), templateData, (e) => {
               if (e) throw e;
               console.log('LICENSE created');
             });
@@ -104,18 +105,18 @@ program
 
       // Generate README.md
       fs.readFile(
-        path.join(__dirname, 'resources/README.ejs'),
+        path.join(__dirname, '../assets/README.ejs'),
         'utf8',
         (err, data) => {
           if (err) throw err;
-          const newData = ejs.render(data, {
+          const readmeData = ejs.render(data, {
             name,
             description,
             author,
             year: new Date().getFullYear(),
             license: license.toUpperCase(),
           });
-          fs.writeFile(path.join(location, 'README.md'), newData, (e) => {
+          fs.writeFile(path.join(location, 'README.md'), readmeData, (e) => {
             if (e) throw e;
             console.log('README.md created');
           });
